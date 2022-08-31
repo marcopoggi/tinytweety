dbe3b6b87fb9964c4c6f048ceeb3a7158d19b0e0
Marco Poggi <marcopoggi2017@gmail.com>
2022-08-31 19:43:45 -0300

    test - views test: setup & contact test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,31 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Tinitweety"
+  end
+
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
-    assert_select 'title', text: 'Home | Tinitweety'
+    assert_select "title", text: "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select 'title', text: 'Help | Tinitweety'
+    assert_select "title", text: "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', text: 'About | Tinitweety'
+    assert_select "title", text: "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", text: "Contact | #{@base_title}"
   end
 end
