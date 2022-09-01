616608eccb17d4814fe4f5103c6260c55a2e079c
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-01 14:49:37 -0300

    edit - change base title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(title = "")
-    base_title = "Tinitweety"
+    base_title = "TiniTweety"
     title = title.empty? ? base_title : "#{title} | #{base_title}"
   end
 end
