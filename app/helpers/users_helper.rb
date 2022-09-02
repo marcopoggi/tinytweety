b6ce38da44a1af13b3f9d3332dc2cd6f87b15dac
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-02 22:48:34 +0000

    feat - create helper for profile image(gravatar)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?s=200"
+    image_tag(gravatar_url, alt: user.name, class: "user-gravatar")
+  end
 end
