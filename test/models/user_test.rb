64fed96d0735209b47805e1aa999b874562c5b1d
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-02 17:34:43 +0000

    test - add validations test for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,31 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "x" * 245 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@random.COM A_user-example@exam.ple.com user+other@random.cm]
+
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should be rejected for invalid email addresses" do
+    invalid_addresses = %w[user@example,com user.com user@example. @example.com user@exam+ple.com]
+
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup #duplicate user instance with same attributes
+    duplicate_user.email = @user.email.upcase
+
+    @user.save
+    
+    assert_not duplicate_user.valid?
+  end
 end
