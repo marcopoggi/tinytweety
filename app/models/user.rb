ae7e35ef1089ef01271ca7fecaeb73f8cd094fc6
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-02 18:01:29 +0000

    feat - add secure password + validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 256 },
                     format: { with: VALID_EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: {minimum:8, maximum:128}
 end
