de22a190a66f225a6fe332aabcb9cb5a2737dd4d
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-02 17:32:25 +0000

    feat - add validations & before save callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: {maximum:50}
-  validates :email, presence: true, length: {maximum:256}
+  before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 256 },
+                    format: { with: VALID_EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
 end
