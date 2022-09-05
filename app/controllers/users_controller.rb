e8d8fc53016f07d826043609c99c29217afdbb20
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-05 18:00:04 -0300

    feat - add flash message (success signup)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "🙋‍♀️ Welcome to TinyTweety"
+      redirect_to @user #GET /users/:id
     else
       render :new, status: 403
     end
