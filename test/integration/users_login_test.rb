155f11035d238f9cf55dd524067b0eae1a66d542
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-06 20:08:08 +0000

    test - create test for login with valid credentials

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:anonymus)
+  end
+
   test "login with invalid credentials" do
     get login_path
     assert_template "sessions/new"
@@ -9,5 +13,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?, "flash hash should be not empty"
     get root_path
     assert flash.empty?, "flash hash should be empty"
+  end
+
+  test "login with valid credentials" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: "password" } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template "users/show" #profile
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
