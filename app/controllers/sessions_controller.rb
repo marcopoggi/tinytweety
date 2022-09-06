9758ae291ef34888477261a3ec6bf6b24d2c665d
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-06 19:27:12 +0000

    feat - session controller Basic cookies auth (with session method)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #redirect to user profile
+      log_in(user)
+      redirect_to user
+    else
+      #display invalid credentials message
+      flash.now[:danger] = "Invalid email or password"
+      render :new, status: 401
+    end
+  end
+
+  def destroy
+  end
 end
