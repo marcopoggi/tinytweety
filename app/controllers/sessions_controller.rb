9380fe43b79124f6ba5b5dbfccf14118fe04d86b
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-07 00:27:20 +0000

    feat - remember credentials on sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #redirect to user profile
       log_in(user)
+      remember(user)
       redirect_to user
     else
       #display invalid credentials message
