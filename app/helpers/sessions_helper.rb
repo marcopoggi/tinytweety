cbe97e593d46b1d0713a5099c6e5c62ae2d6ca29
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-07 00:25:00 +0000

    feat - add remember method - set virtual field - signing cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,16 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  #remember user in a persistent session
+  def remember(user)
+    #call remember method in User model
+    user.remember #set remember_token virtual field with a new token (BASE64)
+
+    #signing the cookie
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
