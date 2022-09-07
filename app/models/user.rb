9461a9112e3162bd73bfb60f6abed51913c82bf1
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-07 00:24:11 +0000

    feat - add methods for remember,verify,forget and generate session_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  include BCrypt
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
 
   VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -11,7 +14,26 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 8, maximum: 512 }
 
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+    cost = ActiveModel::SecurePassword.min_cost ? Engine::MIN_COST : Engine.cost
+    Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  #return true if token matches with remember_digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    Password.new(remember_digest) == remember_token
+  end
+
+  def forget_token
+    update_attribute(:remember_digest, nil)
   end
 end
