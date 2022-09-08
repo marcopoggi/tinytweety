bab4c184917f3ffe42fbc7d914802f164221e627
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-08 19:57:24 +0000

    feat - keep previous values ​​in case of empty fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
 
+  #in case email or name has blank
+  before_validation :define_empty_fields, on: :update
+
   VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -11,7 +14,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 8, maximum: 512 }
+  validates :password, presence: true, length: { minimum: 8, maximum: 512 }, allow_nil: true
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? Engine::MIN_COST : Engine.cost
@@ -35,5 +38,13 @@ class User < ApplicationRecord
 
   def forget_token
     update_attribute(:remember_digest, nil)
+  end
+
+  #Private methods
+  private
+
+  def define_empty_fields
+    self.email = email_was if email.blank?
+    self.name = name_was if name.blank?
   end
 end
