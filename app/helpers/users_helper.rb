18d66febc5191abadc4f602bb9102c21707176b3
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-09 19:40:39 +0000

    edit - change avatar styles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,8 @@
 module UsersHelper
-  def gravatar_for(user, options = { size: 120 })
+  def gravatar_for(user, options = { size: 150 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    content_tag(:div, class: "user-gravatar-container") do
-      image_tag(gravatar_url, alt: user.name, class: "user-gravatar")
-    end
+    image_tag(gravatar_url, alt: user.name, class: "user-gravatar")
   end
 end
