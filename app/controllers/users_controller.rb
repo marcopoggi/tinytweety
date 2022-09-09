6e6ea0a83072d48838a45a98478720c27e79bd33
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-09 20:58:49 +0000

    feat - only admin can exec destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action(:signup_in_session, only: :new)
-  before_action(:logged_in_user, only: [:index, :edit, :update])
+  before_action(:logged_in_user, only: [:index, :edit, :update, :destroy])
   before_action(:correct_user, only: [:edit, :update])
+  before_action(:admin_user, only: [:destroy])
 
   def index
     @users = User.order(:name).page(params[:page]).per(15)
@@ -41,6 +42,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted successfully"
+    redirect_to users_path
+  end
+
   private
 
   def user_params
@@ -58,6 +65,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to root_url unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 
   def signup_in_session
