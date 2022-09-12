2b648f1d6d75be237766fe0bf471e83b0c819e35
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-12 23:11:26 +0000

    test - integration (signup + activate account)

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                     password_confirmation: "pass1234" }
 
     @user_to_log_in = users(:anonymus)
+    ActionMailer::Base.deliveries.clear
   end
 
   test "invalid signup information" do
@@ -29,22 +30,36 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "span.sign-error"
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path #GET /signup
-
     assert_template "users/new"
 
     assert_difference "User.count", 1 do
       post users_path,
            params: { user: @valid_user }
-      assert_response :redirect
-      follow_redirect!
     end
 
+    assert_emails 1
+    user = assigns(:user)
+    assert_not user.activated?
+
+    #log in before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    #invalid token
+    get edit_account_activation_path("randomtoken")
+    assert_not is_logged_in?
+    #valid token, wrong mail
+    get edit_account_activation_path(user.activation_token, email: "randomemail")
+    assert_not is_logged_in?
+    #valid activation
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
     assert_template "users/show"
     assert is_logged_in?
-    assert_select "div.alert-success>span", text: "🙋‍♀️ Welcome to TinyTweety"
-    assert_select "div.alert-success>button.popup-close", text: "❌"
+    # assert_select "div.alert-success>span", text: "🙋‍♀️ Welcome to TinyTweety"
+    # assert_select "div.alert-success>button.popup-close", text: "❌"
   end
 
   test "should be close session if want register" do
