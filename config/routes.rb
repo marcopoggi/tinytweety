6c717d7de596d5c6021db0c541370784e73e8b48
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-12 23:38:27 +0000

    config - add resource route recovery password

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   #settings
   get "users/:id/settings/account", to: "users#edit", as: "settings_account_user"
