250d8bbf902de2fb7e0f3684cb42843e6a8558e4
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-12 22:58:44 +0000

    feat - verify activated?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in(user)
-      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      #redirect to user profile
-      redirect_back_or(user)
+      if user.activated?
+        log_in(user)
+        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+        #redirect to user profile
+        redirect_back_or(user)
+      else
+        flash[:warning] = "Account not activated, check your email"
+        redirect_to root_path
+      end
     else
       #display invalid credentials message
       flash.now[:danger] = "Invalid email or password"
