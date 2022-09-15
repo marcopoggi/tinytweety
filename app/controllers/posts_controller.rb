f6ffe29a323e669e0a526af3f3ed17349d527993
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-15 15:32:09 +0000

    feat - add action filter/destroy & private method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action(:logged_in_user, only: [:create, :destroy])
+  before_action(:correct_user, only: :destroy)
 
   def create
     @post = current_user.posts.build(post_params)
@@ -13,11 +14,22 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    flash[:info] = "✅ Post Deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    if @post.nil?
+      flash[:info] = "⚠️ The post could not be deleted"
+      redirect_to root_url
+    end
   end
 end
