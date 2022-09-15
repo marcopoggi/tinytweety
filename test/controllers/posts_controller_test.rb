e7bd760cbcbbd262dabd9a52f20b23ac1cb152e4
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-15 00:37:40 +0000

    test - post controller

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = posts(:tau_manifesto)
+  end
+
+  test "should redirect when trying to create without login" do
+    assert_no_difference "Post.count" do
+      post posts_url, params: { post: { content: "Lorem ipsum" } }
+    end
+
+    assert_redirected_to login_url
+  end
+
+  test "should redirect when trying to destroy without login" do
+    assert_no_difference "Post.count" do
+      delete post_url(@post)
+    end
+
+    assert_redirected_to login_url
+  end
 end
