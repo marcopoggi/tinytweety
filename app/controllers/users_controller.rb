471b03dccccf68bdbcd761715a0145cbec8dae3b
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-15 00:20:11 +0000

    edit - remove method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,14 +57,6 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  def logged_in_user
-    unless logged_in?
-      store_location #save current url
-      flash[:danger] = "Please log in"
-      redirect_to login_url, status: 302
-    end
-  end
-
   def correct_user
     @user = User.find(params[:id])
     redirect_to root_url unless current_user?(@user)
