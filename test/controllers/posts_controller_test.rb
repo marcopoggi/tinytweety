2e44a8eb68c93323c5cdb7e021e769b31f43460f
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-15 15:32:43 +0000

    test - delete invalid post

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -20,4 +20,13 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to login_url
   end
+
+  test "should redirect(/) when trying to delete an invalid post" do
+    log_in_as(users(:random))
+    post = posts(:tone)
+    assert_no_difference "Post.count" do
+      delete post_path(post)
+    end
+    assert_redirected_to root_url
+  end
 end
