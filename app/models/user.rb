db5c67c1891a21e8a0878d8cdc4f0049799357ca
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-16 19:51:34 -0300

    feat - add intermediate table "Relationship"

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
 
   has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
+
   before_save { self.email = email.downcase }
   before_create :create_activation_digest
 
