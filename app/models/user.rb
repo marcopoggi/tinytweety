7a50e5bb2438eeeb5c6c2f25323409bd05b79260
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-19 17:26:17 +0000

    edit - subquery for a large number of posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -81,7 +81,8 @@ class User < ApplicationRecord
 
   #Posts
   def feed
-    Post.where("user_id = ?", self.id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: self.id)
   end
 
   #Follow Actions
