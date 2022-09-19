dd1328c5e1357b95e3114cb471f6ffc29b4f29ee
Marco Poggi <marcopoggi2017@gmail.com>
2022-09-19 17:25:36 +0000

    edit - paginate of publications

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @post = current_user.posts.build
-      @feed_posts = current_user.feed.take(3)
+      @feed_posts = current_user.feed.page(params[:page]).per(15)
     end
   end
 
